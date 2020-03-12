ab3fc7d1e71cf933431150d6c529d760f1331ba5
pranshu2hike <pranshu@hike.in>
2020-03-12 15:10:05 +0530

    changed data path

--- train.py
+++ train.py
@@ -382,7 +382,7 @@ if __name__ == '__main__':
 
     tt.arg.device = 'cuda:0' if tt.arg.device is None else tt.arg.device
     # replace dataset_root with your own
-    tt.arg.dataset_root = '/data/private/dataset'
+    tt.arg.dataset_root = './data/'
     tt.arg.dataset = 'mini' if tt.arg.dataset is None else tt.arg.dataset
     tt.arg.num_ways = 5 if tt.arg.num_ways is None else tt.arg.num_ways
     tt.arg.num_shots = 1 if tt.arg.num_shots is None else tt.arg.num_shots
